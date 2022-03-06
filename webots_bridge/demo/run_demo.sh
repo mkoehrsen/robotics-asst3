77f84efe2d8d73f0d0aae83dd838e946520591e1
Jared R. White <jaredwhite128@gmail.com>
2022-03-06 11:37:22 -0500

    Add ROS/Webots bridge code and a compatible Webots world.

---
+++ webots_bridge/demo/run_demo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -xe
+
+# we don't have readlink -f, and we can't count on having coreutils from brew
+DEMO_DIR=$(cd "$(dirname "$0")" && pwd)
+BASE_DIR="$DEMO_DIR"/..
+
+PORT=9999
+
+TMP_SPACE=$(mktemp -d /tmp/ros_webots_demo.XXXXXXXX)
+VENV="$TMP_SPACE"/pyenv
+python3 -m venv "$VENV"
+"$VENV"/bin/pip install --upgrade pip "$BASE_DIR"/python_webots_bridge
+
+# not using tags here since we don't want this demo to have side effects
+# --iidfile didn't work, oddly
+echo "Building docker image silently, please wait"
+DOCKER_IMG=$(nerdctl.lima build -q "$BASE_DIR")
+
+# start the ROS side
+nerdctl.lima run -i --rm -p $PORT:$PORT -v "$DEMO_DIR":/tmp/demo "$DOCKER_IMG" bash << EOF &
+    roscore &
+    python3 -u /usr/local/bin/ros_endpoint.py 9999 &
+    python3 -u /tmp/demo/demo_robot.py
+    exit
+EOF
+
+echo "Waiting a few seconds for ROS to start"
+sleep 5
+
+# start the Webots side
+WEBOTS_HOME=/Applications/Webots.app
+env \
+    DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH":"$WEBOTS_HOME"/lib/controller \
+    PYTHONPATH=$PYTHONPATH:"$WEBOTS_HOME"/lib/controller/python39_brew \
+    "$VENV"/bin/webots_endpoint.py localhost $PORT
---
+++ webots_bridge/python_webots_bridge/src/webots_bridge/devices.py
@@ -0,0 +1,7 @@
+device_list = [
+    "CAMERA",
+    "LEFT_MOTOR",
+    "RIGHT_MOTOR"
+]
+
+devices = {name:ix for (ix, name) in enumerate(device_list)}
